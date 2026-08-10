c61acc21793bd7e8a7edc506d53d67f618797196
kumaylalrahal2009-debug <kumayl.alrahal2009@gmail.com>
2026-08-10 15:53:37 +1000

    Preparing updater recovery

--- server.js
+++ server.js
@@ -1,9 +1,1 @@
 const http = require('node:http');
-const fs = require('node:fs');
-const path = require('node:path');
-const crypto = require('node:crypto');
-const { spawn } = require('node:child_process');
-const { DatabaseSync } = require('node:sqlite');
-
-// Emergency bridge loader. The full Kivo core is bundled in server-core.js.
-require('./server-core.js');
